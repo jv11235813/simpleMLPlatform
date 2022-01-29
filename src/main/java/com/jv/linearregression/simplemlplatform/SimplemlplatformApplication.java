8acc998ca3e8d3d83557621d00e89ef36ade8e87
i397284 <i397284@student.fontys.nl>
2022-01-29 13:00:18 +0100

    Initial project structure

---
+++ src/main/java/com/jv/linearregression/simplemlplatform/SimplemlplatformApplication.java
@@ -0,0 +1,13 @@
+package com.jv.linearregression.simplemlplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplemlplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplemlplatformApplication.class, args);
+	}
+
+}
